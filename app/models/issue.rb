6c55ab126119caa2d8db952df67095b9d22c4883
fatmahussein <fartunhussein346@gmail.com>
2025-05-26 22:22:38 +0300

    Define allowed issue statuses and add presence and inclusion validations for Issue attributes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,8 @@
 class Issue < ApplicationRecord
   belongs_to :project
+  STATUSES = ['New', 'In Progress', 'Closed']
+
+  validates :title, :status, :priority, presence: true
+  validates :status, inclusion: { in: STATUSES }
+  validates :priority, inclusion: { in: 1..5 }
 end
