dc956b8ca47a5be3eed2bf0b7d12bb4a8cfc3098
fatmahussein <fartunhussein346@gmail.com>
2025-05-27 09:18:48 +0300

    Update IssuesController for nested routing and CRUD

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,70 +1,55 @@
 class IssuesController < ApplicationController
-  before_action :set_issue, only: %i[ show edit update destroy ]
+  before_action :set_project
+  before_action :set_issue, only: %i[show edit update destroy]
 
-  # GET /issues or /issues.json
   def index
-    @issues = Issue.all
+    @project = Project.find(params[:project_id])
+    @issues = @project.issues
   end
 
-  # GET /issues/1 or /issues/1.json
   def show
   end
 
-  # GET /issues/new
   def new
-    @issue = Issue.new
+    @issue = @project.issues.build
   end
 
-  # GET /issues/1/edit
+  def create
+    @issue = @project.issues.build(issue_params)
+    if @issue.save
+      redirect_to project_issue_path(@project, @issue), notice: 'Issue created successfully.'
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
-  # POST /issues or /issues.json
-  def create
-    @issue = Issue.new(issue_params)
-
-    respond_to do |format|
-      if @issue.save
-        format.html { redirect_to @issue, notice: "Issue was successfully created." }
-        format.json { render :show, status: :created, location: @issue }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /issues/1 or /issues/1.json
   def update
-    respond_to do |format|
-      if @issue.update(issue_params)
-        format.html { redirect_to @issue, notice: "Issue was successfully updated." }
-        format.json { render :show, status: :ok, location: @issue }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @issue.errors, status: :unprocessable_entity }
-      end
+    if @issue.update(issue_params)
+      redirect_to project_issue_path(@project, @issue), notice: 'Issue updated successfully.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /issues/1 or /issues/1.json
   def destroy
-    @issue.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to issues_path, status: :see_other, notice: "Issue was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @issue.destroy
+    redirect_to project_issues_path(@project), notice: 'Issue deleted successfully.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_issue
-      @issue = Issue.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def issue_params
-      params.expect(issue: [ :title, :status, :priority, :project_id ])
-    end
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_issue
+    @issue = @project.issues.find(params[:id])
+  end
+
+  def issue_params
+    params.require(:issue).permit(:title, :status, :priority)
+  end
 end
